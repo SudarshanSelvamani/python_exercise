3480482a1f05256a3200db8d18c125d5fb5fcc8e
Sudarshan <sudarshan@testpress.in>
2021-08-24 14:11:01 +0530

    divisors

---
+++ divisors.py
@@ -0,0 +1,9 @@
+def divisor(number):
+    divisors = []
+    half_of_number = number//2+1
+    for x in range(2, half_of_number):
+        if number%x == 0:
+            divisors.append(x)
+    return divisors
+
+print(divisor(26))
