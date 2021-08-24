308a39c3a872cc06749cfa1175f4da3b97fba889
Sudarshan <sudarshan@testpress.in>
2021-08-24 12:38:49 +0530

    character_input

---
+++ character_input.py
@@ -0,0 +1,5 @@
+name = input("What is your name: ")
+age = int(input("How old are you: "))
+current_year = 2020
+year = str((current_year - age)+100)
+print(name + " will be 100 years old in the year " + year)
